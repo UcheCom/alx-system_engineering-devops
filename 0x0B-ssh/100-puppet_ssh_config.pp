fad66a69afaf5c3b725c5fa639d298834b9e3201
Uchenna Oko <uche4newton1981@gmail.com>
2023-11-26 23:15:06 +0100

    Puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Making changes to configuration file using Puppet
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+}
