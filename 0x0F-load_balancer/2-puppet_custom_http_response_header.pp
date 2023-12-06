c284f91f4cd7c476f859abbb0aba71100abd7748
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-06 18:05:29 +0100

    custom HTTP with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# This installs Nginx and custom HTTP response headers
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => '\tadd_header X-serverd-By ${hostname};',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
