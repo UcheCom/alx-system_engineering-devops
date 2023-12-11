a1496661ce5a45aa00a06580a35bc6a23a83ee90
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-11 22:49:01 +0100

    custom HTTP header response, but with Puppet.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,21 @@
 # This installs Nginx and custom HTTP response headers
 
-package { 'nginx':
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'add_header':
+-> file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
   line   => '\tadd_header X-served-By ${hostname};',
+  after  => 'server_name _;',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+-> file { '/var/www/html/index.html':
+  content => 'Holberton School!!',
 }
 
-service { 'nginx':
+-> service { 'nginx':
   ensure  => running,
 }
