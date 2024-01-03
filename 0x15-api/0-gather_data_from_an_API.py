4264d3ca595187966e63e8f805b71e37b29a7cc9
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-03 23:44:53 +0100

    Adding Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This returns a to-do list information for a given employee ID."""
-import sys
 import requests
+import sys
 
 if __name__ == '__main__':
+    id = int(sys.argv[1])
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(id)).json()
     print("Employee {} is done with tasks".format(user.get('name')),
           end="")
 
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": id}).json()
     task = []
     for t in todos:
         if t.get('completed'):
