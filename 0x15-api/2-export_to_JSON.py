ce65227b3e490338a8ef8a3046a1c0a16250b36b
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-03 17:04:37 +0100

    Adding Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,11 +12,14 @@ if __name__ == '__main__':
     USERNAME = user.get('username')
 
     todos = requests.get(url + "todos", params={"userId": USER_ID}).json()
-
+    dic_t = {USER_ID: []}
+    for t in todos:
+        TASK_COMPLETED_STATUS = t.get('completed')
+        TASK_TITLE = t.get('title')
+        dic_t[USER_ID].append({
+                              "task": TASK_TITLE,
+                              "completed": TASK_COMPLETED_STATUS,
+                              "username": USERNAME})
     filename = '{}.json'.format(USER_ID)
     with open(filename, 'w') as jsonfil:
-        json.dump({USER_ID: [{
-                "task": t.get('title'),
-                "completed": t.get('completed'),
-                "username": USERNAME
-            } for t in todos]}, jsonfil)
+        json.dump(dic_t, jsonfil)
