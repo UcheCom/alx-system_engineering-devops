46883606fc256af4c3ce10811a0debefa1d87ed6
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-04 13:48:51 +0100

    Adding Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,20 +6,15 @@ import requests
 import sys
 
 if __name__ == '__main__':
+    user_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
-    name = user.get('username')
+    user = requests.get(url + 'users/{}'.format(user_id)).json()
+    username = user.get('username')
+    tasks = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    task1 = []
-    for t in todos:
-        task1.append([sys.argv[1], name,
-                      t.get('completed'),
-                      t.get('title')])
-
-    filename = '{}.csv'.format(sys.argv[1])
-    with open(filename, 'w') as csvfil:
+    filename = "'{}.csv'.format(user_id)"
+    with open(filename, 'w', newline="") as csvfil:
         writer = csv.writer(csvfil, quoting=csv.QUOTE_ALL)
-
-        for t in task1:
-            writer.writerow(t)
+        [writer.writerow(
+             [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in tasks]
