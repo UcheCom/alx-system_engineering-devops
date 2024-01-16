8a5bf05bb8c49740d545708de1432a8ec6cbbcc1
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-16 15:35:28 +0100

    recurse api

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Defines recursive function that queries the Reddit API and
+    returns a list of hot articles
+"""
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """queries the all articles title"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {'User-Agent': 'api_advanced-project'}
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 50
+    }
+    response = get(url, headers=headers, params=params,
+                   allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get('data')
+    after = results.get('after')
+    count += results.get('dist')
+    for child in results.get('children'):
+        hot_list.append(child.get('data').get('title'))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
