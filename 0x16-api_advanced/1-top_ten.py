f075bdc215a0346ef93e396dee8b30645f99f845
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-16 14:33:59 +0100

    url error fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ from requests import get
 
 def top_ten(subreddit):
     """ Prints the first 10 hot posts listed"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     params = {'limit': 10}
     user = {'User-agent': 'Uchecm1'}
     response = get(url, headers=user, params=params)
