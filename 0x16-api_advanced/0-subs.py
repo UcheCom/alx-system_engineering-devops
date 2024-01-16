a0f19776c27faa3dcb902233faf8f53546b2cd6c
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-16 13:52:39 +0100

    returns the number of subscriber

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Defines the function that queries the Reddit API and returns
+   the number of subscribers.
+"""
+from sys import argv
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ This returns the number subscribers,
+       returns 0 if the request is invalid
+    """
+    url = 'https://www.reddit.com/r/{}/about.json.format(subreddit)'
+    user = {'User-agent': 'Lizzie'}
+    response = get(url, headers=user)
+    resp = response.json()
+
+    try:
+        return resp.get('data').get('subscribers')
+    except Exception:
+        return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
