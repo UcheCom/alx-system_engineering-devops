1b112e985b903164a067a3a41a62a24b1fe39f10
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-25 11:54:57 +0100

    Fixed failed requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increase the ULIMIT of the default file
+
+exec { 'nginx-fix':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
