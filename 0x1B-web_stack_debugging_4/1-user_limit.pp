88472a8c9ca035c641ac4167cb624f2358bd29ff
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-25 12:11:50 +0100

    Fixed login and file opening issues

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@
 # Increase the hard file limit for the Holberton users
 
 exec { 'holberton-hard-file-fix':
-  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/',
 }
 
 # Increase the soft file limit for the Holberton users
 
 exec { 'holberton-soft-file-fix':
-  command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",  path    => '/usr/local/bin/:/bin/', 
+  command => "sed -i '/^holberton soft/s/4/50000/' /etc/security/limits.conf",  path    => '/usr/local/bin/:/bin/', 
 }
